52520b57b185ed4ed72efa8bcaa5f8776b1172e2
OUREAlexandra <OUREAlexandra@users.noreply.github.com>
2020-12-05 23:58:00 +0100

    adde CategoryType and ProgramType with their forms

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 Quetes 9 et 10
 https://www.loom.com/share/e4d623a81d8a45bb99de3b7a2c199ea0 
+
+
+Quete 11
+https://www.loom.com/share/3018c04dd5d142169eeea2bdaf1e11a8
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -9,6 +9,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use App\Entity\Program;
 use App\Entity\Season;
 use App\Entity\Episode;
+use App\Form\ProgramType;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
 * @Route("/programs", name="program_")
@@ -29,6 +31,35 @@ class ProgramController extends AbstractController
             'program/index.html.twig',
             ['programs' => $programs],
         );
+    }
+
+    /**
+     * The controller for the program add form
+     *
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request) : Response
+    {
+        // Create a new Program Object
+        $program = new Program();
+        // Create the associated Form
+        $form = $this->createForm(ProgramType::class, $program);
+        // Get data from HTTP request
+        $form->handleRequest($request);
+        // Was the form submitted ?
+        if ($form->isSubmitted()) {
+            // Deal with the submitted data
+            // Get the Entity Manager
+            $entityManager = $this->getDoctrine()->getManager();
+            // Persist Program Object
+            $entityManager->persist($program);
+            // Flush the persisted object
+            $entityManager->flush();
+            // Finally redirect to programs list
+            return $this->redirectToRoute('program_index');
+        }
+        // Render the form
+        return $this->render('program/new.html.twig', ["form" => $form->createView()]);
     }
 
     /**
