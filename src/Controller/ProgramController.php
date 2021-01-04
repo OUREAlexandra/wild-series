e8e53346540c1e46e4fa01c0dd8020c0db585dda
OUREAlexandra <OUREAlexandra@users.noreply.github.com>
2021-01-05 00:55:47 +0100

    mailer added

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -11,6 +11,8 @@ use App\Entity\Season;
 use App\Entity\Episode;
 use App\Form\ProgramType;
 use App\Service\Slugify;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -39,7 +41,7 @@ class ProgramController extends AbstractController
      *
      * @Route("/new", name="new")
      */
-    public function new(Request $request, Slugify $slugify) : Response
+    public function new(Request $request, Slugify $slugify,  MailerInterface $mailer): Response
     {
         // Create a new Program Object
         $program = new Program();
@@ -58,6 +60,21 @@ class ProgramController extends AbstractController
             $entityManager->persist($program);
             // Flush the persisted object
             $entityManager->flush();
+
+            $email = (new Email())
+
+                ->from($this->getParameter('mailer_from'))
+
+                ->to('your_email@example.com')
+
+                ->subject('Une nouvelle série vient d\'être publiée !')
+
+                ->html($this->renderView('program/newProgramEmail.html.twig', 
+                ['program' => $program]));
+
+
+        $mailer->send($email);
+
             // Finally redirect to programs list
             return $this->redirectToRoute('program_index');
         }
